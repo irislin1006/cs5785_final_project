af1b87a75a7d1a64732cf72501a8de92f4163a63
林冠言 <linguanyan@MacBook-Pro-4.local>
2019-12-05 11:42:00 -0500

    code

---
+++ model.py
@@ -0,0 +1,49 @@
+"""
+this code is modified from https://github.com/ksenialearn/bag_of_words_pytorch/blob/master/bag_of_words-master-FINAL.ipynb
+"""
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+class Ingres2Recipe(nn.Module):
+    """
+    BagOfWords classification model
+    """
+    def __init__(self, vocab_size, emb_dim, img_fea_size, dropout):
+        """
+        @param vocab_size: size of the vocabulary.
+        @param emb_dim: size of the word embedding
+        """
+        super(Ingres2Recipe, self).__init__()
+        # pay attention to padding_idx
+        self.embed = nn.Embedding(vocab_size, emb_dim, padding_idx=0)
+
+        self.linear_img1 = nn.Linear(img_fea_size, 200)
+        self.linear_img2 = nn.Linear(200, 100)
+        self.linear_i1 = nn.Linear(emb_dim, 200)
+        self.linear_i2 = nn.Linear(200, 100)
+
+        self.dropout = nn.Dropout(dropout)
+        self.batchnorm = nn.BatchNorm1d(200)
+
+    def forward(self, img_fea, captions, c_lengths):
+        """
+        @param recipes, ingres: matrix of size (batch_size, max_sentence_length). Each row in data represents a
+            review that is represented using n-gram index. Note that they are padded to have same length.
+        @param r_length, i_length: an int tensor of size (batch_size), which represents the non-trivial (excludes padding)
+            length of each sentences in the data.
+        """
+        imgf = F.relu(self.linear_img1(img_fea))
+        imgf = self.batchnorm(imgf)
+        imgf = self.dropout(imgf)
+        imgf = self.linear_img2(imgf)
+
+        cap = self.embed(captions)
+        cap = self.dropout(cap)
+        cap = torch.sum(cap, dim=1)
+        cap /= i_length.float() + 1e-10
+        cap = F.relu(self.linear_i1(cap))
+        cap = self.dropout(cap)
+        cap = self.linear_i2(cap)
+
+        return imgf, cap
---
+++ preprocess.py
@@ -0,0 +1,82 @@
+import os
+import pickle
+import json
+import argparse
+import numpy as np
+import re
+import pandas as pd
+
+def preprocess_train(path):
+    img_path = os.path.join(path, 'features_train/')
+    cap_path = os.path.join(path, 'descriptions_train/')
+    tag_path = os.path.join(path, 'tags_train/')
+    
+    img_filename = img_path + 'features_resnet1000intermediate_train.csv'
+    images_df = pd.read_csv(img_filename, header=None)
+    images = images_df.values
+    img_ids, img_features = images[:, 0], images[:, 1:]
+    print(img_ids.shape)
+    print(img_features.shape)
+    print(img_features[0, 0])
+    print(type(img_features[0, 0]))
+    
+    output = []
+    for i, (id_p, img_f) in enumerate(zip(img_ids, img_features)):
+        data = {}
+        id = id_p.split('/')[1]
+        filename = id[:-4]
+        cap_filename = cap_path + filename +'.txt'
+        tag_filename = tag_path + filename +'.txt'
+
+        data['image_id'] = id
+        data['image_2048'] = img_f
+        data['captions'] = open(cap_filename, 'r').read().splitlines()
+        data['tags'] = open(tag_filename, 'r').read().splitlines()
+        
+        output.append(data)
+
+    length = len(output)
+    train, val = output[:int(length*0.8)], output[int(length*0.8):]
+    print(len(train))
+    print(len(val))
+    with open('train.pkl', 'wb') as outfile:
+        pickle.dump(train, outfile)
+    with open('val.pkl', 'wb') as outfile:
+        pickle.dump(val, outfile)
+
+def preprocess_test(path):
+    img_path = os.path.join(path, 'features_test/')
+    cap_path = os.path.join(path, 'descriptions_test/')
+    tag_path = os.path.join(path, 'tags_test/')
+    
+    img_filename = img_path + 'features_resnet1000intermediate_test.csv'
+    images_df = pd.read_csv(img_filename, header=None)
+    images = images_df.values
+    img_ids, img_features = images[:, 0], images[:, 1:]
+    
+    output = []
+    for i, (id_p, img_f) in enumerate(zip(img_ids, img_features)):
+        # print(img_f.shape)
+        data = {}
+        id = id_p.split('/')[1]
+        filename = id[:-4]
+        cap_filename = cap_path + filename +'.txt'
+        tag_filename = tag_path + filename +'.txt'
+
+        data['image_id'] = id
+        data['image_2048'] = img_f
+        data['captions'] = open(cap_filename, 'r').read().splitlines()
+        data['tags'] = open(tag_filename, 'r').read().splitlines()
+        
+        output.append(img_features)
+
+    # print(output)
+    print(images.shape)
+    with open('test.pkl', 'wb') as outfile:
+        pickle.dump(output, outfile)
+
+
+if __name__ == '__main__':
+    data_path = './data'
+    preprocess_train(data_path)
+    preprocess_test(data_path)
